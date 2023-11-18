9c54bc7b9c622586e88df7e6c727ecf7d1c6dbec
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-11-18 14:38:04 +0200

    init homework_3 project

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 + [Work in console 1](homework_1)
 + [Work in console 2](homework_2)
++ [BlackJack](homework_3)
---
+++ homework_3/BlackJack/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAccount
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+        
+        }
+    }
+}
